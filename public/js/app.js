084ca199ff17800b435458a35316d075f28052ee
Daniel Manfrin <danielmanfrin@MBPdiDalManfrin.home-life.hub>
2021-12-29 17:18:39 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,13 +13,11 @@ weatherForm.addEventListener("submit", (e) => {
   messageTwo.textContent = "";
 
   const location = search.value;
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) return (messageOne.textContent = data.error);
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forecastData;
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) return (messageOne.textContent = data.error);
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecastData;
+    });
+  });
 });
